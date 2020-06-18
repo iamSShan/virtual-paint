ec9874e557b6ef1b015735cf53098c177e319b34
Shantanu Sharma <shantanusharma197@gmail.com>
2020-06-18 14:05:05 +0530

    Added config.ini

---
+++ utils.py
@@ -0,0 +1,14 @@
+
+# Sample colors which can be detected for now
+COLORS_HSV_VALUES = [[94,105,2,121,255,255],  # Blue # Order is: HueMin, SatMin, ValMin, HueMax, SatMax, ValMax
+                    [0,133,100,179,255,255],  # Red
+                    [25,52,60,102,255,255]]  # Green
+
+# BGR Values of colors
+DRAWING_COLORS = [[204, 102, 0],   # Blue 
+                 [0, 0, 204],   # Red
+                 [0, 128, 0]]  # Green
+
+
+def to_int(string_value):
+	return int(string_value)
